5aeb3bd91a7f832e430ffea4b869941191d6d6e6
wgranger <twgranger@gmail.com>
2015-10-22 00:51:28 -0500

    Initial Commit of Two Ruby Challenges

---
+++ week-4/smallest-integer/my_solution.rb
@@ -0,0 +1,26 @@
+# Smallest Integer
+
+# I worked on this challenge [by myself, with: ].
+
+# smallest_integer is a method that takes an array of integers as its input
+# and returns the smallest integer in the array
+#
+# +list_of_nums+ is an array of integers
+# smallest_integer(list_of_nums) should return the smallest integer in +list_of_nums+
+#
+# If +list_of_nums+ is empty the method should return nil
+
+# Your Solution Below
+def smallest_integer(list_of_nums)
+  if list_of_nums.empty?
+  	return nil
+  else
+  	small_num = list_of_nums[0]
+  	list_of_nums.each do |num|
+  		if num < small_num 
+  			small_num = num
+  		end
+  	end
+  	return small_num
+  end
+end
