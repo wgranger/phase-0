32b06153250b4bfc44fd5259127431355afe8dc9
wgranger <twgranger@gmail.com>
2015-11-12 09:48:37 -0600

    Draft of Eloquent File

---
+++ week-7/eloquent.js
@@ -0,0 +1,34 @@
+// Eloquent JavaScript
+
+// Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
+
+// Program Structure
+// Write your own variable and do something to it.
+var addition = 16
+addition = addition + 11
+console.log(addition)
+
+//Write a program that asks the user for their favorite food
+var favoriteFood = prompt("What is your favorite food?")
+console.log("Hey! That's my favorite too!")
+
+// Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+
+var hash = "#";
+var counter = 0;
+while (counter < 7){
+	console.log(hash);
+	hash = hash + "#";
+	counter += 1;
+}
+
+// Functions
+
+// Complete the `minimum` exercise.
+
+function min(x, y){
+console.log(Math.min(x, y));
+};
+
+// Data Structures: Objects and Arrays
+// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
