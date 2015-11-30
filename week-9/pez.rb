bac38df11db948d7058596bb5d040bb3fa1efb45
wgranger <twgranger@gmail.com>
2015-11-29 20:08:31 -0600

    Add Pez Solution

---
+++ week-9/pez.rb
@@ -0,0 +1,84 @@
+# PezDispenser Class from User Stories
+
+# I worked on this challenge [by myself, with: ].
+# I spent [#] hours on this challenge.
+
+
+# 1. Review the following user stories:
+# - As a Pez user, I'd like to be able to "create" a new Pez dispenser with a group of flavors that
+#      represent Pez so it's easy to start with a full Pez dispenser.
+# - As a Pez user, I'd like to be able to easily count the number of Pez remaining in a dispenser
+#      so I can know how many are left.
+# - As a Pez user, I'd like to be able to take a Pez from the dispenser so I can eat it.
+# - As a Pez user, I'd like to be able to add a Pez to the dispenser so I can save a flavor for later.
+# - As a Pez user, I'd like to be able to see all the flavors inside the dispenser so I know the order
+#      of the flavors coming up.
+
+
+# Pseudocode
+
+
+
+# Initial Solution
+
+class PezDispenser
+  attr_accessor :number
+  attr_accessor :contents
+  
+  def initialize(flavors)
+    array = []
+    x = 1
+    while x < 13 do
+    array << flavors[rand(flavors.length)]
+      x+=1
+    end
+    @contents = array
+    @number = @contents.length
+  end
+  
+  def pez_count
+    @contents.length
+  end
+  
+  def see_all_pez
+    p @contents
+  end
+  
+  def add_pez(taste)
+    @contents.push(taste)
+    @number +=1
+  end
+  
+  def get_pez
+    return @contents.pop
+  end
+  
+end
+
+
+
+# Refactored Solution
+
+
+
+
+
+
+# DRIVER TESTS GO BELOW THIS LINE
+
+flavors = %w(cherry chocolate cola grape lemon orange peppermint raspberry strawberry).shuffle
+super_mario = PezDispenser.new(flavors)
+puts "A new pez dispenser has been created. You have #{super_mario.pez_count} pez!"
+puts "Here's a look inside the dispenser:"
+puts super_mario.see_all_pez
+puts "Adding a banana pez."
+super_mario.add_pez("banana")
+puts "Now you have #{super_mario.pez_count} pez!"
+puts "Oh, you want one do you?"
+puts "The pez flavor you got is: #{super_mario.get_pez}"
+puts "Now you have #{super_mario.pez_count} pez!"
+
+
+
+
+# Reflection
